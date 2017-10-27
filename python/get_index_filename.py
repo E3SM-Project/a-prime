be7d8e85e02b99b3f1dd7515618b59ceb06e78ea
Milena Veneziani <milena@lanl.gov>
2017-10-26 20:54:08 -0700

    Added license headers to atm enso files

--- python/get_index_filename.py
+++ python/get_index_filename.py
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2017, UT-BATTELLE, LLC
+# All rights reserved.
+# 
+# This software is released under the BSD license detailed
+# in the LICENSE file in the top level a-prime directory
+#
 from get_season_name import get_season_name
 
 
