e9d9eca4adb10e24df07f4df32b3792b29db08ae
Xylar Asay-Davis <xylarstorm@gmail.com>
2018-03-03 15:46:21 -0700

    Remove MPAS-Analysis submodule

--- bash_scripts/aprime_ocnice_diags.bash
+++ bash_scripts/aprime_ocnice_diags.bash
@@ -2,25 +2,12 @@
 #
 # Copyright (c) 2017, UT-BATTELLE, LLC
 # All rights reserved.
-# 
+#
 # This software is released under the BSD license detailed
 # in the LICENSE file in the top level a-prime directory
 #
 
 # GENERATE OCEAN DIAGNOSTICS
-
-# Initialize MPAS-Analysis code
-export GIT_DISCOVERY_ACROSS_FILESYSTEM=true
-export tmp_currentdir="`pwd`"
-export tmp_gittopdir="`git rev-parse --show-toplevel`"
-cd $tmp_gittopdir
-#
-git submodule update --init
-#
-echo
-echo "MPAS-Analysis submodule: "`git submodule status`
-cd $tmp_currentdir
-unset tmp_currentdir tmp_gittopdir
 
 export config_file="$log_dir/config.ocnice.$uniqueID"
 python python/setup_ocnice_config.py
